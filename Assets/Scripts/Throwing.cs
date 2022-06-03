85c7924620e61384e7413b4b3fe49d0e68ae8f8f
tastybird107 <wallyholbeck@gmail.com>
2022-06-02 23:32:08 -0500

    Throwing shit

--- Assets/Scripts/Throwing.cs
+++ Assets/Scripts/Throwing.cs
@@ -10,15 +10,64 @@ public class Throwing : MonoBehaviour
     public Transform attackPoint;
     public GameObject thrownObject;
 
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.Mouse0;
+    public float throwForce;
+    public float throwUpwardForce;
+
+    bool readyToThrow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        readyToThrow = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0)
+        {
+            Throw();
+        }
+    }
+
+    private void Throw()
+    {
+        readyToThrow = false;
+
+        //Instantiate object to throw
+        GameObject projectile = Instantiate(thrownObject, attackPoint.position, cam.rotation);
+
+        //Get rigidbody component
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        // calculate direction
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        //Add force
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalThrows--;
+
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
     }
 }
